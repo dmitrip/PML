c59a5f37f7d20e0e9fadecefbf1742de639d500d
Dmitri Pavlichin <dmitripa@gmail.com>
2017-10-13 12:01:21 -0700

    added julia folder

--- python/pml.py
+++ python/pml.py
@@ -1,105 +1,32 @@
+# created by Dmitri S. Pavlichin in October 2017
+
 import numpy as np
 np.seterr(invalid='ignore') 
-np.seterr(divide='ignore') # for case 0/0
+#np.seterr(divide='ignore') # for case 0/0
 from scipy import special # for gammaln
-import warnings
+#import warnings
+import collections # for Counter
 
-def estimate_entropy_PML_approximate(samp,K=None):
-    # estimates entropy of distribution given sample samp
-    # optionally given assumed true support set size K
-    #
-    # approximates pattern maximum likelihood (PML) distribution p
-    # where p \approx \arg \max_p' P(unordered histogram)
-    # Approximation sums over all permutations that mix within blocks of
-    # constant p
-    #
-    # Uses approximate PML distribution as plug-in for entropy functional
-    #
-    # created by Dmitri S. Pavlichin on May 31, 2017
-    #
-    # Args:
-    #     * samp - (vector) sample, vector of non-negative integers
-    # Optional args:
-    #     * K - (integer) assumed support set size.  
-    #         If K is not provided, then we attempt to estimate the support set size
-    #
-    # Returns:
-    #     * H_est - (double) estimate for entropy, log base 2
-    #     * K_est - (integer) estimate for support set size
-    #
-    # Example:
-
-    # get empirical histogram
-    hist_vec = int_hist(samp)
-
-    # find PML distribution
-    (p_approx,F0_est,_,not_have_valid_K_est) = PML_distribution_approximate(hist_vec,K)
-
-    # estimate support set size
-    K_est = K if K is not None else np.sum(hist_vec > 0) + F0_est
-
-    # plug-in to entropy functional
-    H_est = entropy_of_distribution(p_approx, 2)
-
-    return H_est
-
-def estimate_Renyi_entropy_PML_approximate(samp,alpha,K=None):
-    # estimates Renyi entropy with parameter alpha of distribution given sample samp
-    # optionally given assumed true support set size K
-    #
-    # approximates pattern maximum likelihood (PML) distribution p
-    # where p \approx \arg \max_p' P(unordered histogram)
-    # Approximation sums over all permutations that mix within blocks of
-    # constant p
-    #
-    # Uses approximate PML distribution as plug-in for Renyi entropy functional
-    #
-    # created by Dmitri S. Pavlichin on May 31, 2017
-    #
-    # Args:
-    #     * samp - (vector) sample, vector of non-negative integers
-    #     * alpha - (double) Renyi entropy parameter, alpha >= 0, alpha != 1
-    # Optional args:
-    #     * K - (integer) assumed support set size.  
-    #         If K is not provided, then we attempt to estimate the support set size
-    #
-    # Returns:
-    #     * H_est - (double) estimate for Renyi entropy, log base 2
-    #     * K_est - (integer) estimate for support set size
-    #
-    # Example:
-
-    # get empirical histogram
-    hist_vec = int_hist(samp)
-
-    # get empirical histogram
-    hist_vec = int_hist(samp)
-
-    # find PML distribution
-    (p_approx,F0_est,_,not_have_valid_K_est) = PML_distribution_approximate(hist_vec,K)
-
-    # estimate support set size
-    K_est = K if K is not None else np.sum(hist_vec > 0) + F0_est
-
-    # plug-in to Renyi entropy functional
-    RenyiH_est = renyi_entropy_of_distribution(p_approx, alpha, 2)
-
-    return RenyiH_est
-
-
-def entropy_of_distribution(vec,base=2.0):
-    # computes Shannon entropy of vector vec, default base 2
-    Z = np.sum(vec)
-    H = sum(-p*np.log(p) for p in vec if p > 0.0)
-    return (H/Z + np.log(Z))/np.log(base)
-
-def renyi_entropy_of_distribution(vec, alpha, base=2.0):
-    # computes Renyi entropy of vector vec, parameter alpha, default base 2
-    Z = np.sum(vec)
-    return (np.log(np.sum(np.power(p/Z,alpha) for p in vec))/(1-alpha)) / np.log(base)
+def counts(sample):
+    """get histogram of sample, ordered arbitrarily
+    
+    Args:
+        sample - list
+    
+    Returns:
+        numpy array h, where h[i] = |{t: sample[t] == i}|
+        
+    Examples:
+        >>> counts([0,0,1,'cat',1,1])
+        [2, 3, 1]
+    
+    """
+    
+    _, h = np.unique(sample, return_counts=True)
+    return h
 
 def int_hist(sample):
-    """get integer histogram of all integer values in range(np.max(sample))
+    """get integer histogram of all integer values in range(np.max(sample)+1)
     
     Args:
         sample - integer-valued list
@@ -117,11 +44,12 @@ def int_hist(sample):
 
 def cumsum_mat(x):
     """computes cumultative sum matrix
+    x is numpy array
     C[i,j] = sum(x[i:j+1])
 
     """    
     K = np.size(x)
-    C = np.zeros((K,K))
+    C = np.zeros((K,K), dtype=x.dtype)
     for i in range(K):
         C[i,i] = x[i]
         for j in range(i+1,K):
@@ -129,199 +57,217 @@ def cumsum_mat(x):
     
     return C
 
-def PML_distribution_approximate(p_empirical, K=None, warn_on_continuous_part=True):
-    # approximates pattern maximum likelihood (PML) distribution p_approx
-    # where p_approx \approx \arg \max_p P_p(unordered p_empirical)
-    # Approximation sums over all permutations that mix within blocks of
-    # constant p_approx
-    #
-    # created by Dmitri S. Pavlichin on May 14, 2017
-    #
-    # Args:
-    #     * p_empirical - (integer-valued vector) empirical histogram, entries sum to sample size
-    # Optional args:
-    #     * K - (integer) assumed support set size, must have K >= sum(p > 0).  
-    #         If K is not provided, then we attempt to estimate the support set size
-    #     * warn_on_continuous_part - (bool) true iff warn if inferred distribution has continuous part
-    # #     * K_upper_bound - (integer) assumed maximum value for support set
-    #         size, use when estimating support set size.  If K_upper_bound is
-    #         not provided, then we set K_upper_bound = sum(p == 1)^2
-    #
-    # Returns:
-    #     * p_approx - (column vector) approximate PML distribution, sorted in
-    #         descending order
-    #     * F0_est - (integer) estimate for number of symbols seen 0 times in
-    #         p_empirical.  If K is provided as an argument, then F0_est = max(K - sum(p > 0), 0)
-    #     * V_approx - (double) approximate value of log(P_p(unordered p_empirical))
-    #         achieved by p = p_approx
-    #     * reached_max_F0 - (boolean) true iff we did not find an upper bound
-    #        when estimating support set size, hit upper bound sum(p > 0) + sum(p == 1)^2
-    #
-    # Example:
-    #     Optimize support set size:
-    #         >> p_approx = PMLdistributionApproximate([9,3,2,1,1])
-    #         yields p_approx == [9/16 7/80 7/80 7/80 7/80 7/80]'
-    #     OK to have zeros in input histogram, zeros are ignored:
-    #         >> p_approx = PMLdistributionApproximate([9 3 2 1 1 0 0 0 0 0 0]')
-    #         yields p_approx == [9/16 7/80 7/80 7/80 7/80 7/80]'
-    #     Assumed support set size:
-    #         >> p_approx = PMLdistributionApproximate([9 3 2 1 1]',5)
-    #         yields p_approx == [9/16 7/64 77/6/64 7/64 7/64]'
+def maximize_on_interval_int(fun, x_min, x_max, tol=1e-14):
+    """maximizes semiconcave function fun, integer arguments, on interval [x_min, x_max]
 
-    def f0(V, counts, multiplicities, F0, n, num_bins, B):
-        Vmax = special.gammaln(num_bins+F0+1) + n*np.log(1/(num_bins+F0))
-        imax = 0
-        for i in range(B):
-            c = np.sum(counts[i+1:])
-            m = np.sum(multiplicities[i+1:])
+    created by Dmitri S. Pavlichin on May 14, 2017
 
-            if c > 0:
-                proposal_reward = V[i] + special.gammaln(m+F0+1) + c*np.log((c/n)/(m+F0))
-            else:
-                proposal_reward = V[i] + special.gammaln(m+F0+1)
+    Args:
+        * fun - function to maximize
+        * x_min - interval min
+        * x_max - interval max
 
-            if proposal_reward > Vmax:
-                Vmax = proposal_reward
-                imax = i+1
+    Optional:
+        * tol (float) - tolerance optimize
 
-        return (Vmax,imax)
+    Assumptions:
+        * interval [x_min, x_max] includes at least one integer
+
+    Returns:
+        (x, val)
+            * x (int) - maximizing value of x
+            * val - fun(x)
+
+    """
+
+    # check assumptions
+    assert np.ceil(x_min) <= np.floor(x_max), '[x_min, x_max] interval must include at least one integer'
+
+    while np.floor(x_max) > np.floor(x_min):
+        x1 = (x_max - x_min)/3 + x_min
+        x2 = 2*(x_max - x_min)/3 + x_min
+        V1 = fun(x1)
+        V2 = fun(x2)
+        if V1 - V2 <= -np.abs(tol):
+            x_min = x1
+        else:
+            x_max = x2
+
+    x = x_min
+    if fun(np.floor(x)) >= fun(np.ceil(x)):
+        x = np.floor(x_min)
+    else:
+        x = np.ceil(x_min)
+
+    return (x, fun(x))
+
+def harmonic_number(n):
+    # harmonic number H_n
+    return np.sum(1/i for i in range(1,n+1))
+
+def ML_unseen_symbols_uniform(T, n):
+    """maximum likelihood estimate for number of unseen symbols given empirical
+    support set size T, n samples
+
+    created by Dmitri S. Pavlichin on October 2, 2017
+
+    Args:
+        * T (int) - empirical support set size
+        * n (int) - sample size
+
+    Returns:
+        * F0 (int) - maximum likelihood estimator for number of unseen symbols
+
+    """
+
+    # check for F0 == 0
+    if n >= T*harmonic_number(T):
+        F0 = 0
+    else:
+        # check for F0 == inf
+        if T >= n:
+            F0 = np.inf
+        else:
+            fun = lambda f0: special.gammaln(f0+T+1)-special.gammaln(f0+1)-n*np.log(f0+T)
+            F0 = maximize_on_interval_int(fun, 0, np.ceil((T*T-n)/(n-T))+1)[0]
+
+    return F0
+
+def approximate_PML_from_histogram(p_empirical, K=None):
+    """
+    approximates pattern maximum likelihood (PML) distribution p_approx
+    where p_approx \approx \arg \max_p P_p(unordered p_empirical)
+    Approximation sums over all permutations that mix within blocks of
+    constant p_approx
+    
+    created by Dmitri S. Pavlichin on May 14, 2017
+    
+    Args:
+        * p_empirical - (integer-valued vector) empirical histogram, entries sum to sample size
+    Optional args:
+        * K - (integer) assumed support set size, must have K >= sum(p > 0).  
+            If K is not provided, then we attempt to estimate the support set size
+    
+    Returns:
+        * p_approx - (column vector) approximate PML distribution, sorted in
+            descending order
+        * F0_est - (integer) estimate for number of symbols seen 0 times in
+            p_empirical.  If K is provided as an argument, then F0_est = max(K - sum(p > 0), 0)
+        * V_approx - (double) approximate value of log(P_p(unordered p_empirical))
+            achieved by p = p_approx
+    
+    Example:
+        Optimize support set size:
+            >>> p_approx = approximate_PML_from_histogram([9,3,2,1,1])
+            returns p_approx == [9/16 7/80 7/80 7/80 7/80 7/80]'
+        OK to have zeros in input histogram, zeros are ignored:
+            >>> p_approx = approximate_PML_from_histogram([9 3 2 1 1 0 0 0 0 0 0]')
+            returns p_approx == [9/16 7/80 7/80 7/80 7/80 7/80]'
+        Assumed support set size:
+            >>> p_approx = approximate_PML_from_histogram([9 3 2 1 1]',5)
+            returns p_approx == [9/16 7/64 77/6/64 7/64 7/64]'
+
+    """
 
     p_empirical = np.array(p_empirical)
+    p_empirical = p_empirical[p_empirical != 0] # remove 0-counts
     
-    fing = int_hist(p_empirical[p_empirical>0])
-    bin_values = np.flipud(np.where(fing>0)[0]) # sort in descending order
-    multiplicities = fing[bin_values]
-    num_bins = np.sum(multiplicities)
+    fing = int_hist(p_empirical)
+    fing_supp = np.where(fing>0)[0] # sorted in ascending order
+    fing_values = fing[fing_supp]
 
-    B = bin_values.size # multibins
-    n = np.dot(bin_values, multiplicities)
-    n_1 = np.sum(p_empirical==1) # number of symbols observed once
+    B = fing_supp.size # multibins, F_+
+    n = np.dot(fing_supp, fing_values) # bin counts times fing_values, sum(bm) = sum(p)
 
-    # early detect case n_1 == n?
+    # early detect case all symbols observed once?
 
     # get counts and multiplicities cumsum matrix
     # n[i,j] = sum_{k = i to j} n[k]
-    counts = bin_values*multiplicities
+    counts = fing_supp*fing_values
     counts_mat = cumsum_mat(counts)
-    mults_mat = cumsum_mat(multiplicities)
+    mults_mat = cumsum_mat(fing_values)
     prob_mat = counts_mat/(n*mults_mat) # probability within each block
 
+    # C[i,j] = log reward assign multibins from i to j to same prob
     log_reward_mat = special.gammaln(mults_mat+1) + counts_mat*np.log(prob_mat)
-    log_reward_mat[np.isnan(log_reward_mat)] = 0.0
+    log_reward_mat[np.isnan(log_reward_mat)] = 0.0 # handles 0 log 0 = 0 case, sets below diagonal entries to 0
 
     # dynamic programming
     # V[i] = best log reward for first i bins
     V = np.zeros(B) # % V[-1] = 0, values matrix
-    backpointers = np.zeros(B, dtype=int) # backpointers to get best log reward
-    for i in range(B):
-        V[i] = log_reward_mat[0,i]
-        for j in range(i):
-            proposal_reward = V[j] + log_reward_mat[j+1,i]
+    backpointers = np.zeros(B, dtype=int)-1 # backpointers to get best log reward
+    for i in range(B-1,-1,-1):
+        V[i] = log_reward_mat[i,B-1]
+        backpointers[i] = B-1
+        for j in range(i,B-1):
+            proposal_reward = log_reward_mat[i,j] + V[j+1]
             if proposal_reward > V[i]:
                 V[i] = proposal_reward
-                backpointers[i] = j+1
+                backpointers[i] = j
 
-    ## optimize alphabet size
-    # get upper bound on F0
-    reached_max_F0 = False
-    if K is None:
-        F0 = 0 # extra 0 entries
-        step_size = 1
-        F0_max = num_bins**2
-        V0 = f0(V, counts, multiplicities, 0, n, num_bins, B)[0] - special.gammaln(0+1)
-
-        done = False
-        while not done:
-            F0 += step_size
-
-            V0_prev = V0
-            V0 = f0(V, counts, multiplicities, F0, n, num_bins, B)[0] - special.gammaln(F0+1)
-
-            if V0_prev >= V0:
-                done = True
-            else:
-                step_size *= 2
-                if F0 > F0_max:
-                    reached_max_F0 = True
-                    done = True
-        F0_max = F0
-
-        # get optimum value of F0
-        if not reached_max_F0:
-            F0_lower = 0
-            F0_upper = F0_max
-            while F0_lower != F0_upper:
-                F1 = (F0_upper-F0_lower)//3+F0_lower
-                F2 = 2*(F0_upper-F0_lower)//3+F0_lower+1
-                V1 = f0(V, counts, multiplicities, F1, n, num_bins, B)[0] - special.gammaln(F1+1)
-                V2 = f0(V, counts, multiplicities, F2, n, num_bins, B)[0] - special.gammaln(F2+1)
-                if V1 - V2 <= -1e-10:
-                    F0_lower = F1+1
-                else:
-                    F0_upper = F2-1
-            F0 = F0_lower
+    ## merge 0s with rest of symbols
+    V0_opt = -np.inf # optimal value
+    F0_opt = -1
+    i0_opt = -1 # index of optimal fingerprint bin up to which to merge 0s
+    for i in range(B):
+        T = np.sum(fing_values[:(i+1)])
+        N = np.sum(fing_supp[:(i+1)]*fing_values[:(i+1)])
+        if K is not None: # assumed alphabet size
+            F0 = K - len(p_empirical)
         else:
-            F0 = F0_max
-
-        # test to see if have continuous part
-        if bin_values[-1] == 1:
-            V_non_cont = f0(V, counts, multiplicities, F0, n, num_bins, B)[0] - special.gammaln(F0+1)
-            if bin_values.size > 1:
-                V_cont = V[-2] + n_1*np.log(n_1/n)
-            else:
-                V_cont = 0.0
-            if V_cont > V_non_cont:
-                if warn_on_continuous_part:
-                    warnings.warn('optimal distribution has continuous part, F0 = inf')
-                F0 = np.inf
-    else:
-        F0 = K - num_bins
-        if F0 < 0:
-            F0 = 0
-    F0_est = F0
-
-    if not np.isinf(F0):
-        (Vmax,imax) = f0(V, counts, multiplicities, F0, n, num_bins, B)
-        backpointers = np.append(backpointers, imax)
-        bin_values = np.append(bin_values, 0)
-        multiplicities = np.append(multiplicities, F0)
-        B += 1
-        num_bins += F0
-        counts = bin_values*multiplicities
-        counts_mat = cumsum_mat(counts)
-        mults_mat = cumsum_mat(multiplicities)
-        prob_mat = counts_mat/(n*mults_mat) # probability within each block
-    else:
-        Vmax = V_cont
-        backpointers = backpointers[:-1]
-        bin_values = bin_values[:-1]
-        multiplicities = multiplicities[:-1]
-        B -= 1
-        num_bins -= n_1
-        counts = bin_values*multiplicities
-        counts_mat = cumsum_mat(counts)
-        mults_mat = cumsum_mat(multiplicities)
-        prob_mat = counts_mat/(n*mults_mat) # probability within each block
+            # optimize alphabet size
+            # estimate number of unseen symbols for uniform distribution, N samples, empirical support set size T
+            F0 = ML_unseen_symbols_uniform(T, N)
+        # get value
+        V0 = N*np.log(N/n)
+        if i < B-1:
+            V0 += V[i+1] # V[B] = 0
+        if not np.isinf(F0): # \lim_{F0 -> \infty} f(F0,T,n=T) = 0
+            V0 += (special.gammaln(F0+T+1) - special.gammaln(F0+1) - N*np.log(F0+T))
+        # check if best so far
+        if V0 > V0_opt:
+            V0_opt = V0
+            F0_opt = F0
+            i0_opt = i
+    F0 = F0_opt
+    V_approx = V0_opt
 
     ## get assignment
-    p_approx = np.zeros(num_bins)
+    p_approx = np.zeros_like(p_empirical, dtype=float)
+    if F0 > 0:
+        backpointers[0] = i0_opt # merge 1s with same symbols as 0s, since guaranteed to merge 0s at least with 1s 
+    i = 0
+    while i <= B-1:
+        j = backpointers[i]
+        if i == 0:
+            x1 = 0
+        else:
+            x1 = mults_mat[0,i-1]
+        x2 = mults_mat[0,j]
+        p_approx[x1:x2] = prob_mat[i,j]
+        i = j + 1
+    continuous_part = 0 # probability mass of continuous part
+    # add 0s in front
+    if F0 > 0:
+        x = mults_mat[0,i0_opt] # number of seen symbols in same level set as unseen symbols
+        if not np.isinf(F0):
+            p_approx = np.insert(p_approx, 0, np.zeros(int(F0)))
+            p_approx[0:(x+int(F0))] = counts_mat[0,i0_opt]/(n*(x+F0))
+        else:
+            continuous_part = counts_mat[0,i0_opt]/n
+            p_approx = p_approx[x:]
 
-    current_multibin = B-1
-    ix = num_bins-1
+    ## get log probability of fingerprint
+    V_approx = V_approx + special.gammaln(n+1) - np.sum(fing_values*special.gammaln(fing_supp+1)) - np.sum(special.gammaln(fing_values+1))
 
-    while ix >= 0:
-        t_prev = current_multibin
-        t = backpointers[current_multibin]
-        while current_multibin >= t:
-            p_approx[(ix-multiplicities[current_multibin]+1):(ix+1)] = prob_mat[t,t_prev]
-            ix -= multiplicities[current_multibin]
-            current_multibin -= 1
+    ## sort PML approximation in descending order
+    p_approx = np.flipud(p_approx)
 
-    ##
+    have_continuous_part = np.isinf(F0)
 
-    V_approx = Vmax + special.gammaln(n+1) \
-        - np.sum(multiplicities*special.gammaln(bin_values+1)) \
-        - np.sum(special.gammaln(multiplicities+1))
-    
-    return (p_approx, F0_est, V_approx, reached_max_F0)
+    #return (p_approx, F0, V_approx, have_continuous_part)
+    return p_approx
+
+def estimate_fun_from_histogram(p_empirical, K=None):
+    pass
 
